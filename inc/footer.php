67353719fc0a2e18652136e2b80f360db74a3574
Firzan <145427772+firzannabeelar@users.noreply.github.com>
2024-05-25 20:05:17 +0700

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <div class="container">
+            <p>&copy; 2024 INGGRIS.IN PARE. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,28 @@
+<?php
+include 'db.php';
+?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Teachers:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rekomendasi Tempat Les Bahasa Inggris di Kampung Inggris</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">Inggris.IN Pare</div>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="recommendations.php">Rekomendasi</a></li>
+                    <li><a href="contact.php">Kontak</a></li>
+                    <li><a href="about.php">Tentang Kami</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
